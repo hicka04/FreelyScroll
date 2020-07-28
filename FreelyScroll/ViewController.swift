40029db2d0a7ed9760dc1bcf89a7fe7eb9f04cdf
hicka04 <hicka04@gmail.com>
2020-07-28 20:59:08 +0900

    Initial Commit

---
+++ FreelyScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreelyScroll
+//
+//  Created by hicka04 on 2020/07/28.
+//  Copyright © 2020 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
